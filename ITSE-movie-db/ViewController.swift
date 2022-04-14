071c84f57efedf2c0d3f7f08db5d86c25f466f0f
Mason Phillips <math.matrix@icloud.com>
2022-04-14 13:26:43 -0500

    Initial Commit

---
+++ ITSE-movie-db/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITSE-movie-db
+//
+//  Created by Mason Phillips on 4/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
